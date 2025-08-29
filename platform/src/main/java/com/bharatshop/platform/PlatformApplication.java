ab1c3dc65d2feff3a1912568c0b216d8171d45fa
sonic7adarsh <user@example.com>
2025-08-29 12:24:15 +0530

    additional functionality

--- platform/src/main/java/com/bharatshop/platform/PlatformApplication.java
+++ platform/src/main/java/com/bharatshop/platform/PlatformApplication.java
@@ -2,12 +2,20 @@ package com.bharatshop.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = {
+    "com.bharatshop.platform.repository",
+    "com.bharatshop.shared.repository"
+})
+@EntityScan(basePackages = {
+    "com.bharatshop.platform.entity",
+    "com.bharatshop.shared.entity"
+})
 @EnableTransactionManagement
 @EnableCaching
 public class PlatformApplication {
